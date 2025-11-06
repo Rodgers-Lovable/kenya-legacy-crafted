97b1aacf26f00f6c48cd0fb2a947bee8242e2fc1
Mawira <brian.mawi@yahoo.com>
2025-11-06 23:29:26 +0300

    change phone number

--- src/core/constants/appConstants.ts
+++ src/core/constants/appConstants.ts
@@ -1,9 +1,9 @@
-export const WHATSAPP_NUMBER = "254712413243";
+export const WHATSAPP_NUMBER = "254712345678";
 
 export const COMPANY_NAME = "Karen Legacy Tours & Safaris";
 export const COMPANY_WEBSITE = "https://karenlegacytoursandsafaris.com";
 
-export const COMPANY_PRIMARY_TEL = "+254 (0) 712 413 243";
+export const COMPANY_PRIMARY_TEL = "+254 (0) 712 345 678";
 
 export const COMPANY_PRIMARY_EMAIL = "info@karenlegacytoursandsafaris.com";
 export const COMPANY_SECONDARY_EMAIL =
@@ -15,3 +15,4 @@ export const COMPANY_ADDRESS = "COMING SOON";
 export const DEVELOPER_NAME = "Mawira";
 export const DEVELOPER_WEBSITE = "https://mawirab.com";
 export const DEVELOPER_EMAIL = "dev@mawirab.com";
+export const DEVELOPER_PHONE = "254712413243";
